5ab6c489a06266f7abd4d7df156a7cacee0ee1ad
Krithi0528 <120370422+Krithi0528@users.noreply.github.com>
2023-10-16 15:23:24 +0530

    Delete Day20 directory

--- Day20/Weather-1/src/main/java/com/example/demo/weather/repository/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.weather.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.weather.entity.FeedbackEntity;
-
-public interface FeedbackRepository extends JpaRepository<FeedbackEntity,Integer>{
-
-}
